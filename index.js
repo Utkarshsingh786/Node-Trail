a58869f04f04be12f0ae367331de5663b3d0fa89
Utkarshsingh786 <us8896664511@gmail.com>
2023-03-02 19:52:41 +0530

    "second commit"

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-console.log("github");
-console.log("hey");
+const fs = require("fs");
+const path = require("path");
+const dirpath = path.join(__dirname, "files");
+for (var i = 0; i < 5; i++) {
+  fs.writeFileSync(`${dirpath}/file${i}.txt`, "A text file");
+}
+
+fs.readdir(dirpath, (err, files) => {
+  files.forEach((item) => {
+    console.log("file name is", item);
+  });
+}); 
